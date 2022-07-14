ae335eefd18323b45632b6cfc0b581f8de69906f
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-07-14 16:36:15 +0530

    Typescript basics
    Signed-off-by: Ghanashyam-Bhat <ghanashyambhat6@gmail.com>

---
+++ typescript/dest/test.js
@@ -0,0 +1,9 @@
+"use strict";
+function greet(name) {
+    if (name)
+        console.log(name.toLocaleUpperCase());
+    else
+        console.log('Hola!');
+}
+greet(null);
+//# sourceMappingURL=test.js.map
